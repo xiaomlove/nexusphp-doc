c5b1adb1f2b08e8db0b1f5a68ed1d477e6cfff57
xiaomlove <353856593@qq.com>
2021-01-06 19:08:24 +0800

    add config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,10 @@
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around'
+  title: 'NexusPHP 使用文档',
+  description: 'Just playing around',
+  themeConfig: {
+    nav: [
+        { text: 'Blog', link: 'http://nexusphp.cn/' },
+        { text: 'Github', link: 'https://github.com/xiaomlove/nexusphp' }
+    ]
+  }
 }
